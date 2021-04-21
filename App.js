f03403b81ea7cc86a3bdcac681640c7b7a392ca3
Sumeeth-24 <sumeethmoolyaa@gmail.com>
2021-04-21 16:03:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PlacesNavigator from './navigation/PlacesNavigator';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import ReduxThunk from 'redux-thunk';
+import placesReducer from './store/places-reducer';
+import {init} from './helpers/db';
+
+init().then(() => {
+  console.log('Initilised successfull');
+}).catch(err => {
+  console.log('Initialised db failed');
+  console.log(err);
+});
+
+const rootReducer = combineReducers({
+  places: placesReducer
+});
+
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
+
+
+export default function App() {
+  return (
+    <Provider store ={store} >
+   <PlacesNavigator />
+   </Provider>
+  );
+}
+
+
